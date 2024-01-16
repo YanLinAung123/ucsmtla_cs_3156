30a89f6a25f1da4c6d37358d4251f77764d0dbdc
YanLinAung123 <yaung2444@gmail.com>
2024-01-16 21:46:31 +0630

    insert item in array

--- chapter_6/exe_5.php
+++ chapter_6/exe_5.php
@@ -6,7 +6,7 @@ foreach($original as $x){
 }
 $inserted="$";
 array_splice($original,3,0,$inserted);
-echo "<br> After inserting '$S' the array is : "."<br>";
+echo "<br> After inserting '$' the array is : "."<br>";
 foreach($original as $x){
     echo "$x";
 }
