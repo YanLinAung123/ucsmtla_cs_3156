c5142c32ce93f59be9d72b0545c201d611628935
YanLinAung123 <yaung2444@gmail.com>
2024-01-16 20:36:24 +0630

    array example

---
+++ chapter_6/exe_3.php
@@ -0,0 +1,8 @@
+<?php
+    $ceu=array("Italy"=>"Rome","Luxembourg"=>"Luxembourg","Belgium"=>"Brussels","Denmark"=>"Copenhagen","Finland"=>"Helsinki","France"=>"Paris","Slovakia"=>"Bratislava","Slovenia"=>"Ljubjana","Germany"=>"Berlin","Greece"=>"Athens","Ireland"=>"Dublin","Netherlands"=>"Amsterdam","Portugal"=>"Lisbon","Spain"=>"Madrid","Sweden"=>"Stockholm","United Kingdom"=>"London","Cyprus"=>"Nicosia","Lithuania"=>"Vilius","Czech Republic"=>"Prague","Estonia"=>"Tallin","Hungary"=>"Budapest","Latvia"=>"Riga","Malta"=>"Valetta","Austria"=>"Vienna","Poland"=>"Warsaw");
+
+    asort($ceu);
+
+    foreach($ceu as $country=>$capital){
+        echo "The Capital of $country is $capital";
+    }
